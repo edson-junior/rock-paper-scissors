fc13a4e70604e38d2721c338d7c848bfab2dc733
Edson Domingos Junior <soneddjr@gmail.com>
2017-06-06 00:24:29 +0200

    install bootstrap

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const app = express();
 
 app.use(compression());
 app.use("/public", express.static(__dirname + '/public'));
+app.use("/public/bootstrap", express.static(__dirname + '/node_modules/bootstrap/dist/css/'));
 
 app.set('port', (process.env.PORT || 3000));
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => res.render(path.join(__dirname, 'public/src/views/index')));
+app.get("/", (req, res) => res.render(path.join(__dirname, 'public/src/views/index'),
+  { site_title: "Rock, Paper, Scissors (lizard, spock!!!)" }));
 
 app.listen(app.get('port'));
 
