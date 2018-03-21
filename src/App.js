0390b2708e66d03353a44e27a83912eaea33c190
Edson Junior <edson-junior@g-star.com>
2018-03-21 15:03:51 +0100

    convert vanillajs app into react app

--- src/App.js
+++ src/App.js
@@ -3,69 +3,74 @@ import Players from './components/Players'
 import Score from './components/Score'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-let result = null
-const result_a = "paper wins"
-const result_b = "scissors wins"
-let matches = 0
+const result_a = 'paper wins'
+const result_b = 'scissors wins'
 
-export const PLAYER_A = {
-  'choice': "paper",
-  'points': 0,
-  'points_wrapper': '.player-a-points'
+const playerA = {
+  points: 0
 }
 
-export const PLAYER_B = {
-  'choice': null,
-  'points': 0,
-  'points_wrapper': '.player-b-points'
-}
-
-export const compare = (a, b) => {
-  matches++
-
-  if (a === b) {
-    result = "tie"
-    return
-  }
-
-  if (a === "paper") {
-    if (b === "rock") {
-      result = result_a
-      return PLAYER_A.points = PLAYER_A.points + 1
-    } else {
-      if (b === "scissors") {
-        result = result_b
-        return PLAYER_B.points = PLAYER_B.points + 1
-      }
-    }
-  }
+const playerB = {
+  points: 0
 }
 
 class App extends Component {
-  choiceHandler = () => {
-    if (matches >= 1000) return;
+  state = {
+    result: null,
+    matches: 0,
+    playerA: {
+      choice: "paper"
+    },
+    playerB: {
+      choice: null
+    }
+  }
 
-    PLAYER_B.choice = Math.random() * 100;
+  compare = (a, b) => {
+    this.state.matches++
 
-    PLAYER_B.choice < 34 ? PLAYER_B.choice = "rock" :
-    PLAYER_B.choice >= 67 ? PLAYER_B.choice = "paper" :
-    PLAYER_B.choice = "scissors";
+    if (a === b) {
+      this.state.result = "tie"
+      return
+    }
 
-    compare(PLAYER_A.choice, PLAYER_B.choice);
+    if (a === "paper") {
+      if (b === "rock") {
+        this.state.result = result_a
+        return playerA.points = playerA.points + 1
+      } else {
+        if (b === "scissors") {
+          this.state.result = result_b
+          return playerB.points = playerB.points + 1
+        }
+      }
+    }
+  }
 
-    document.querySelector(PLAYER_A.points_wrapper).innerHTML = PLAYER_A.points;
-    document.querySelector(PLAYER_B.points_wrapper).innerHTML = PLAYER_B.points;
-    document.querySelector('.choices .player-b-choice').innerHTML = PLAYER_B.choice;
-    document.querySelector('.round-number').innerHTML = matches;
-    document.querySelector('.round-result').innerHTML = result;
+  choiceHandler = event => {
+    event.preventDefault()
+
+    if (this.state.matches >= 1000) return;
+    
+    let randomChoice = Math.random() * 100
+
+    randomChoice < 34 ? randomChoice = "rock" :
+    randomChoice >= 67 ? randomChoice = "paper" :
+    randomChoice = "scissors"
+
+    this.setState({
+      playerB: { choice: randomChoice }
+    })
+
+    this.compare(this.state.playerA.choice, this.state.playerB.choice);
   }
 
   render() {
     return (
       <form className="container" onSubmit={ this.choiceHandler }>
         <h1 className="page-header">Rock, Paper, Scissors</h1>
-        <Players/>
-        <Score/>
+        <Players playerB={this.state.playerB.choice} />
+        <Score playerA={playerA.points} playerB={playerB.points} roundNumber={this.state.matches} roundResult={this.state.result} />
         <button className="play btn btn-default btn-primary">PLAAAAAAY!!!!</button>
       </form>
     );
--- src/components/Players.js
+++ src/components/Players.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Players extends Component {
-  render() {
-    return (
-      <div className="panel panel-default">
-        <table className="choices table">
-          <thead>
-            <tr>
-              <th>Name: </th>
-              <th className="player player-a">Player A</th>
-              <th className="player player-b">Player B</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Choice: </td>
-              <td className="player player-a-choice">paper</td>
-              <td className="player player-b-choice">random</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    )
-  }
-}
+const Players = ({ playerB }) => (
+  <div className="panel panel-default">
+    <table className="choices table">
+      <thead>
+        <tr>
+          <th>Name: </th>
+          <th className="player player-a">Player A</th>
+          <th className="player player-b">Player B</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Choice: </td>
+          <td className="player player-a-choice">paper</td>
+          <td className="player player-b-choice">{ playerB ? playerB : 'random' }</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+)
 
-export default Players;
+
+export default Players
