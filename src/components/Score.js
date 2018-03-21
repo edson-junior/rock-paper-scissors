9915a92487002d6a884bcfbc1a2551b4b5486943
Edson Junior <soneddjr@gmail.com>
2018-03-21 15:03:51 +0100

    convert vanillajs app into react app

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-class Players extends Component {
+class Score extends Component {
   render() {
     return (
       <div className="panel panel-default">
         <table className="score table">
           <thead>
             <tr>
-              <td colSpan="2"><strong>Round:</strong> <span className="round-number">0</span></td>
+              <td colSpan="2"><strong>Round:</strong> <span className="round-number">{ this.props.roundNumber }</span></td>
             </tr>
             <tr>
-              <td colSpan="2"><strong>Result:</strong> <span className="round-result">0</span></td>
+              <td colSpan="2"><strong>Result:</strong> <span className="round-result">{ this.props.roundResult }</span></td>
             </tr>
             <tr>
               <td><strong>Score</strong></td>
@@ -22,8 +22,8 @@ class Players extends Component {
           </thead>
           <tbody>
             <tr>
-              <td className="player-a player-a-points">0</td>
-              <td className="player-b player-b-points">0</td>
+              <td className="player-a player-a-points">{ this.props.playerA }</td>
+              <td className="player-b player-b-points">{ this.props.playerB }</td>
             </tr>
           </tbody>
         </table>
@@ -32,4 +32,4 @@ class Players extends Component {
   }
 }
 
-export default Players
+export default Score
