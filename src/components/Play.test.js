46358ff4ce87d6a4af093c63b4d206dd08a88e8b
Edson Junior <soneddjr@gmail.com>
2018-06-06 12:00:50 +0200

    start creating dispatch functions

--- src/components/Play.test.js
+++ src/components/Play.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, configure } from 'enzyme';
+import { mount, configure } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import Play from './Play';
 import Adapter from 'enzyme-adapter-react-16';
@@ -9,11 +9,31 @@ configure({ adapter: new Adapter() });
 
 const initialState = [];
 const mockStore = configureStore();
-const props = { store: mockStore(initialState) };
-const play = shallow(<Play { ...props } />);
+const mockOnRock = jest.fn();
+const mockOnPaper = jest.fn();
+const mockOnScissors = jest.fn();
+const props = { 
+  store: mockStore(initialState),
+  onRock: mockOnRock,
+  onPaper: mockOnPaper,
+  onScissors: mockOnScissors
+};
+const play = mount(<Play { ...props } />);
 
 describe('Play', () => {
   it('should render properly', () => {
     expect(toJson(play)).toMatchSnapshot();
   });
+
+  // @TODO: fix the mock function
+  describe('when the user clicks on an item', () => {
+    it('should dispatch rock', () => {
+      play.find('.rock').simulate('click');
+
+      expect(mockOnRock).toHaveBeenCalled();
+    });
+  });
+
+  
+
 });
