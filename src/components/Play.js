ca02915aa13f0d3a04e4ea96419ee6ea87a985f7
Edson Junior <edson-junior@g-star.com>
2018-06-06 12:00:50 +0200

    start creating dispatch functions

--- src/components/Play.js
+++ src/components/Play.js
@@ -4,12 +4,32 @@ import { connect } from 'react-redux';
 import { addRock, addPaper, addScissors } from '../actions/play';
 
 class Play extends Component {
+  static propTypes = {
+    onRock: PropTypes.func.isRequired,
+    onPaper: PropTypes.func.isRequired,
+    onScissors: PropTypes.func.isRequired,
+  }
   render() {
-    return(
-      <div>
-      </div>
+    const { onRock, onPaper, onScissors } = this.props;
+
+    return (
+      <Fragment>
+        <button className="rock" onClick={() => onRock()}>rock</button>
+        <button className="paper" onClick={() => onPaper()}>paper</button>
+        <button className="scissors" onClick={() => onScissors()}>scissors</button>
+      </Fragment>
     );
   }
 }
 
-export default connect()(Play);
+const mapStateToProps = state => ({
+  store: state,
+});
+
+const mapDispatchToProps = dispatch => ({
+  onRock: id => dispatch(addRock(id)),
+  onPaper: id => dispatch(addPaper(id)),
+  onScissors: id => dispatch(addScissors(id)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Play);
