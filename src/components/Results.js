841c10dfbdcda110dca347f85780cc3347f5f32a
Edson Junior <edson-junior@g-star.com>
2018-06-12 09:43:22 +0200

    add jsdoc and proptypes to Results component

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Results = ({ store } = {}) => (
+/**
+ * Render the redux state in the component.
+ * @param {object} state - The redux initial state.
+ * @returns {*} The rendered component.
+ */
+const Results = ({ store }) => (
   <div>
     <h3>Result: You win!</h3>
 
@@ -12,5 +18,9 @@ const Results = ({ store } = {}) => (
     </div>
   </div>
 );
+
+Results.propTypes = {
+  store: PropTypes.shape({}).isRequired,
+};
 
 export default Results;
